6e9245a86fdf14982bbc2d496618aaa9eb2a470b
Vladislav Reznik <vladek_1999@mail.ru>
2023-03-31 21:59:18 +0300

    Initial Commit

---
+++ Magic-8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball
+//
+//  Created by Владислав Резник on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
